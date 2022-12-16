621b7784e0ee8d278e7dff43e8e7dd6a7a317c06
feralferret224 <114872161+feralferret224@users.noreply.github.com>
2022-12-16 17:02:21 -0500

    done

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -17,7 +17,7 @@ function About() {
           <div className='col-8 d-flex flex-column text-start justify-content-start'>
           <h2 className='mb-3 d-flex justify-contents-start title'>Getting to know me</h2> 
           <p>I'm a Brown University student studying computer science, with special emphasis on <b>cloud technologies</b>, <b>design</b>, and <b>learnability</b>. I also enjoy writing, and I've published both nonfiction pieces and satire.</p>
-          <p>I'm from the Chicago area, and in my free time I enjoy listening to Dolly Parton and The Strokes. I'm also a fan of classic films, and I love a good murder mystery.</p>
+          <p>I'm from the Chicago area, and in my free time, I enjoy listening to <a href='https://g.co/kgs/vCU2B8'>Dolly Parton</a> and <a href="https://g.co/kgs/MR1bXZ">The Strokes</a>. I'm also a fan of classic films, and I love a good murder mystery.</p>
           <ul class="nav justify-content-end justify-items-end">
             <li class="nav-item">
               <a class="nav-link active" href="#"><ResumeIcon sx={{ fontSize: 25 }}/>
